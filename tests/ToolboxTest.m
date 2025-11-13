60c9ce69ef16f364726880ba905b2159faeb21d0
ehennestad <ehennestad@gmail.com>
2025-11-13 09:44:50 +0100

    Fixed toolbox test

--- tests/ToolboxTest.m
+++ tests/ToolboxTest.m
@@ -1,16 +1,15 @@
 classdef ToolboxTest <  matlab.unittest.TestCase
 % ToolboxTest - Unit test for testing the toolbox functions.
 
-
     methods (Test)
         function testToolboxDir(testCase)
-            pathStr = matbox.toolboxdir();
+            pathStr = sysutil.toolboxdir();
             testCase.verifyClass(pathStr, 'char')
             testCase.verifyTrue(isfolder(pathStr))
         end
 
         function testToolboxVersion(testCase)
-            versionStr = matbox.toolboxversion();
+            versionStr = sysutil.toolboxversion();
             testCase.verifyClass(versionStr, 'char')
             testCase.verifyTrue(startsWith(versionStr, 'Version'))
         end
